3ddda8f58d9ef01ec033a2d682a09213ac2c30f9
Pratiksha Gotmare <gotmare00@gmail.com>
2026-05-16 18:37:00 +0530

    Add files via upload

---
+++ Day 1/typecasting.py
@@ -0,0 +1,54 @@
+#Simple if
+# print(2+2)
+# print("2"+"2")
+# a= int(input("Enter first number:"))
+# b = int(input("Enter second number:"))
+# print(a+b)
+
+# int() used to convert it in integer 3.14 = int = 3
+# print(int(3.14)) 
+# #print(int(10+5j))  
+# print(int(True))  # = 1
+# print(int(False))   # = 0
+# we can not convert complex value to int
+# we can not convert floating point value string to int
+# can not convert string name to int
+
+#-------------------------------------------------------------------------------#
+
+#float()used to convert
+# print(float(3))  # 3.0
+# #print(float(50+2j))
+# print(float(True))  
+# print(float(False))
+# print(float(4.22))
+# print(float("4"))
+# #print(name("name"))
+# we can not convert complex value to float
+# we can not convert string name to float
+
+#--------------------------------------------------------------------------------#
+
+# complex() used to convert
+# print(complex(3))  
+# print(complex(12.5))
+# print(complex(True))
+# print(complex(False))
+# print(complex("5"))
+# print(complex("5.6"))
+# #print(complex("name"))
+# print(complex(5, -3))
+# print(complex(True, False))
+
+#--------------------------------------------------------------------------------#
+#bool() used to convert
+print(bool(0))  # False
+print(bool(15))  # True
+print(bool(3.14)) # True
+print(bool(0.0))  # False
+print(bool(1+2j)) # True
+print(bool(0+0j)) #False
+print(bool(-1))   #False
+print(bool(False))  #False
+print(bool(True)) #True
+print(bool("")) #False
